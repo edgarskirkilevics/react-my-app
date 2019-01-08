b5b2b84158b26ea02adec29fb5fb052bd01c47f1
edgarskirkilevics <ekirkilevics@gmail.com>
2019-01-08 21:42:01 +0200

    added state and event  handling examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Person/Person';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      {name: "Edgars", age: 28},
+      {name: "Andrejs", age: 24},
+      {name: "Max", age: 20}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    //console.log('Was clicked!');
+    //DONT DO THIS: this.state.persons[0].name = 'Ed';
+    this.setState( {
+        persons : [
+          {name: newName, age: 28},
+          {name: "Andrejs", age: 24},
+          {name: "Max", age: 15}
+        ]
+      }
+    );    
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hi, I am React App</h1>
+       <button onClick={() => this.switchNameHandler('Ed')}>Switch Name</button>
+       <Person 
+       name={this.state.persons[0].name} 
+       age={this.state.persons[0].age}/>
+       <Person 
+       name={this.state.persons[1].name} 
+       age={this.state.persons[1].age}
+       click={this.switchNameHandler.bind(this, 'Edgars!!!!')}>
+       My Hobbies: Learn React</Person>
+       <Person 
+       name={this.state.persons[2].name}
+       age={this.state.persons[2].age}/>
       </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,3 +1,12 @@
-const person = () => {
-    
+import React from 'react';
+
+const person = (props) => {
+    return(
+        <div>
+            <p onClick={props.click}>I am {props.name}! My age is {props.age}</p> 
+            <p>{props.children}</p>
+       </div>
+    );
 }
+
+export default person;
