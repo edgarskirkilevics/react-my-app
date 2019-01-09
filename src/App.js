f73e722f527b2cba33076e6fc4a1024922dcb19d
edgars.kirkilevics <edgars.kirkilevics@accenture.com>
2019-01-09 17:28:57 +0200

    Finished basic stuff

--- src/App.js
+++ src/App.js
@@ -38,10 +38,22 @@ class App extends Component {
   }
 
   render() {
+    //Inline style
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
        <h1>Hi, I am React App</h1>
-       <button onClick={() => this.switchNameHandler('Ed')}>Switch Name</button>
+       <button 
+       style = {style}
+       onClick={() => this.switchNameHandler('Ed')}>Switch Name
+       </button> {/*Prefer to use .bind() */}
        <Person 
        name={this.state.persons[0].name} 
        age={this.state.persons[0].age}/>
